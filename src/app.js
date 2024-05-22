0e215ea7568bd853bd05251ce1ab17152f02a6f1
Сэм <108047432+SamuzDev@users.noreply.github.com>
2024-05-22 12:27:42 -0500

    Update app.js

--- src/app.js
+++ src/app.js
@@ -7,7 +7,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(cors({
-  origin: process.env.CORS_ORIGIN,
+  origin: '*',
   credentials: true,
   methods: 'GET,POST',
 }))
